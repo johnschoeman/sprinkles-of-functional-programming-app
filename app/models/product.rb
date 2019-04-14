256b66dd4663dfa60c83ef24a88174561e76f93c
John Schoeman <johnschoeman1617@gmail.com>
2019-04-14 15:57:21 -0400

    Allow users to upload files of products
    Why:
    We would like for users to be able to upload a csv of product data from
    the products index.
    
    This commit:
    Introduces an upload csv button to the products page, a new imports
    controller, and a class method on Product to handle the import logic. A
    sample fixture was added for a feature test.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,15 @@
+require "csv"
+
 class Product < ApplicationRecord
   validates :name, presence: true
+
+  def self.import(file_path)
+    CSV.foreach(file_path, headers: true) do |row|
+      data = row.to_h
+      data["active"] = data["active"] == "true"
+      data["release_date"] = Time.zone.parse(data["release_date"])
+      product = new(data)
+      product.save
+    end
+  end
 end
