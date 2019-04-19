8b667cefc7a7a0b0494807fda71da8abcf6c4110
John Schoeman <johnschoeman1617@gmail.com>
2019-04-19 07:34:34 -0700

    Introduce product data importer
    Why:
    We would like to have a dedicated service object to handle the logic of
    importing csv files for product data.
    
    This commit:
    Introduces a ProductDataImporter model and moves the import
    functionality from Product.import to
    ProductDataImporter#import

---
+++ spec/models/product_data_importer_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+RSpec.describe ProductDataImporter, type: :model do
+  describe "#import" do
+    context "the file is a csv" do
+      it "saves every row in the file as new product" do
+        require "csv"
+        filepath = stub_csv
+        importer = ProductDataImporter.new(filepath)
+
+        importer.import
+
+        expect(Product.count).to eq 3
+      end
+    end
+  end
+
+  def stub_csv(filename = "filename.csv")
+    file =
+      Tempfile.new(filename).tap do |f|
+        f.puts "name,author,release_date,version,value,active"
+        f.puts "name_a,author_a,20190101,1.0,1,true"
+        f.puts "name_b,author_b,20190201,1.1,2,false"
+        f.puts "name_c,author_c,20190301,1.2,3,true"
+        f.close
+      end
+    file.path
+  end
+end
